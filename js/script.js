cde033e3d7560a47a520f94c380ca35695997dcb
Damon Castillo <damono7.castillo@gmail.com>
2022-06-07 14:26:51 -0500

    update

--- js/script.js
+++ js/script.js
@@ -12,18 +12,43 @@ console.log(movieData());
 
 const renderMovies = () => {
     movieData().then((data) => {
-        let movieCard = data.map((movie) => {
+        let movieCard = data.filter((_, idx) => idx < 5).map((movie) => {
             return `
             <card>
-                <h3>Title: ${movie.id}</h3>
+                <button id="edit" type="button" data-id="${movie.id}">Edit</button>
+                <button id="Delete" type="button" data-id="${movie.id}">X</button>
+                <h3>Title: ${movie.title++}</h3>
             </card>`
 
         })
+
         document.querySelector('#movie-bar').innerHTML = movieCard.join('');
         console.log(movieCard);
+        $("#edit").click(function (){
+            $('.edit-form').toggleClass('hide')
+        })
     })
+
 }
 renderMovies();
+
+const editMovie = (movie) => {
+    const URL = "https://superb-cream-psychiatrist.glitch.me/movies/";
+    let options = {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movie)
+
+    }
+
+    return fetch(`${URL}/${movie.id}`, options).then(resp => resp.json())
+}
+
+
+
+
 
 
 
