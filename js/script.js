ab2318f8dcffea8d17e51941db68c4e0f45a8ccb
orgeron.kevin <orgeron.kevin@gmail.com>
2022-06-07 10:49:39 -0500

    push

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
-// fetch(TESTING).then(res => res.json()).then((x) => {
-//     console.log(x)}).catch((error) =>{
-//     console.error(error);
-// })
+// fetch(URL)
+//     .then(res => res.json())
+//     .then((data) => console.log(data))
+//     .catch((error) =>{console.error(error);
+// });
+
+
+const movieData = () => {
+    return fetch(URL).then(res => res.json());
+}
+console.log(movieData());
+
+const renderMovies = () => {
+    movieData().then((data) => {
+        let movieCard = data.map((movie) => {
+            return `
+            <card>
+                <h3>Title: ${movie.id}</h3>
+            </card>`
+
+        })
+        document.querySelector('#movie-bar').innerHTML = movieCard.join('');
+        console.log(movieCard);
+    })
+}
+renderMovies();
+
+
+
+
+
+
+
+
+
