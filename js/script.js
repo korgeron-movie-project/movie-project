ef146c3d2aafdd15c73200ccb429368758bd450b
orgeron.kevin <orgeron.kevin@gmail.com>
2022-06-07 21:54:25 -0500

    need to finish left button

--- js/script.js
+++ js/script.js
@@ -8,28 +8,68 @@
 const movieData = () => {
     return fetch(URL).then(res => res.json());
 }
-console.log(movieData());
+// console.log(movieData());
+
 
 const renderMovies = () => {
+let i = 5;
     movieData().then((data) => {
         let movieCard = data.map((movie) => {
+
+            // console.log(movie);
+
             return `
             <card>
                 <h3>Title: ${movie.id}</h3>
-            </card>`
-
+            </card>
+            `
         })
-        document.querySelector('#movie-bar').innerHTML = movieCard.join('');
-        console.log(movieCard);
+        let movieBar = document.querySelector('#movie-bar');
+
+
+        const fiveCards = (movies) => {
+            let x = movies.filter((x, index) => index < i)
+            movieBar.innerHTML = x.join('');
+            console.log(movies.length);
+            let buttonR = document.querySelector('buttonRight');
+            buttonR.addEventListener('click', function () {
+                console.log(i)
+                i++;
+                if (i > movieCard.length - 1) {
+                    buttonR.style.display = 'none';
+                }
+                if (movieCard.length >= 5) {
+                    movieBar.firstElementChild.remove();
+                    movieBar.innerHTML += movies[i - 1];
+                }
+
+            })
+            let buttonL = document.querySelector('buttonLeft');
+            buttonL.addEventListener('click', function () {
+                console.log(i)
+                i--;
+                if (i < movieCard.length) {
+                        buttonR.style.display = 'flex';
+                }
+
+                if (movieCard.length >= 5) {
+                    movieBar.lastElementChild.remove();
+                    movieBar.prepend(`<card></card>`);
+                }
+
+            })
+        }
+        fiveCards(movieCard)
+
+
     })
 }
 renderMovies();
 
 
+//TODO: THINGS TO KEEP
 
-
-
-
+// movieBar.innerHTML = movieCard.join('');
 
 
 
