0360d5006462b8e31eaecac020677dfe481037d5
orgeron.kevin <orgeron.kevin@gmail.com>
2022-06-09 21:14:55 -0500

    added movie api

--- js/script.js
+++ js/script.js
@@ -2,105 +2,138 @@ let body = document.querySelector('body');
 body.style.visibility = 'hidden';
 body.classList.add('loadScreen');
 
+setTimeout(function () {
+    document.querySelector('body').style.visibility = 'visible';
+    body.classList.remove('loadScreen');
+}, 1000);
+let counter = 0;
+let searchBar = document.querySelector('#query');
+document.querySelector('#button').addEventListener('click', () => {
+    counter++
 
-const movieData = () => {
-    return fetch(URL).then(res => res.json());
-}
+    let body = document.querySelector('body');
+    body.style.visibility = 'hidden';
+    body.classList.add('loadScreen');
 
-const renderMovies = () => {
-    let click = 0;
-    let i = 5;
-    movieData().then((data) => {
-        console.log(data);
-        let movieCard = data.map((movie) => {
-            return `
-            <card id="${movie.id}">
-                <h3>Title: ${movie.title}</h3>
+
+    const searchMovies = (search) => {
+        console.log(search.value);
+
+        const movieData = () => {
+            return fetch(`http://www.omdbapi.com/?s=${search.value}&page=1&apikey=69918388`).then(res => res.json()).then(data => data.Search);
+        }
+
+        const renderMovies = () => {
+            let click = 0;
+            let i = 5;
+            movieData().then((data) => {
+                console.log(data);
+                const movieTitle = document.querySelector('movieTitle');
+                const rightSide = document.querySelector('rightSide');
+                rightSide.innerHTML = `<img style="height: 280px; width: 100%" src="${data[0].Poster}" alt="failed to load" />`
+                movieTitle.firstElementChild.innerHTML = data[0].Title;
+                if (counter > 1) {
+                    counter = 0;
+                    movieTitle.firstElementChild.innerHTML = '';
+                    movieTitle.firstElementChild.innerHTML = data[0].Title;
+                }
+                let movieCard = data.map((movie) => {
+                    console.log(movie);
+                    return `
+            <card id="${movie.imdbID}">
+                <img style="height: 100%; width: 100%" src="${movie.Poster}" alt="movie-image">
             </card>
             `
-        })
+                })
 
 
-        let movieBar = document.querySelector('#movie-bar');
+                const movieBar = document.querySelector('#movie-bar');
 
 
-        const fiveCards = (movies) => {
-            let x = movies.filter((x, index) => index < i)
-            movieBar.innerHTML = x.join('');
-            let buttonR = document.querySelector('buttonRight');
-            buttonR.addEventListener('click', function () {
-                click++;
-                if (click !== 0) {
-                    buttonL.style.display = 'flex';
-                }
-                i++;
-                if (click < movieCard.length - 1) {
-                    buttonR.style.display = 'none';
-                }
-                if (i < movieCard.length) {
-                    buttonR.style.display = 'flex';
-                }
-                if (movieCard.length >= 5) {
-                    movieBar.firstElementChild.remove();
-                    movieBar.innerHTML += movies[i - 1];
-                }
+                const fiveCards = (movies) => {
+                    let x = movies.filter((x, index) => index < i);
+                    movieBar.innerHTML = x.join('');
+                    let buttonR = document.querySelector('buttonRight');
+                    buttonR.addEventListener('click', function () {
+                        click++;
+                        if (click !== 0) {
+                            buttonL.style.display = 'flex';
+                        }
+                        i++;
+                        if (click < movieCard.length - 1) {
+                            buttonR.style.display = 'none';
+                        }
+                        if (i < movieCard.length) {
+                            buttonR.style.display = 'flex';
+                        }
+                        if (movieCard.length >= 5) {
+                            movieBar.firstElementChild.remove();
+                            movieBar.innerHTML += movies[i - 1];
+                        }
 
-                document.querySelectorAll('card').forEach(function (card) {
-                    console.log(card);
-                    card.addEventListener('click', function () {
-                        console.log(this.id);
+                        document.querySelectorAll('card').forEach(function (card) {
+                            // console.log(card);
+                            card.addEventListener('click', function () {
+                                console.log(this.id);
+                            })
+                        })
+
                     })
-                })
+                    let buttonL = document.querySelector('buttonLeft');
+                    buttonL.style.display = 'none';
+                    buttonL.addEventListener('click', function () {
+                        click--
+                        i--;
+                        if (click === 0) {
+                            buttonL.style.display = 'none'
+                        }
+                        const newFrontCard = () => {
+                            const card = document.createElement('card');
+                            card.innerHTML = movies[i - 5];
+                            return card
+                        }
+                        if (movieCard.length >= 5) {
+                            movieBar.lastElementChild.remove();
+                            movieBar.prepend(newFrontCard().firstElementChild);
 
-            })
-            let buttonL = document.querySelector('buttonLeft');
-            buttonL.style.display = 'none';
-            buttonL.addEventListener('click', function () {
-                click--
-                i--;
-                if (click === 0) {
-                    buttonL.style.display = 'none'
-                }
-                const newFrontCard = () => {
-                    const card = document.createElement('card');
-                    card.innerHTML = movies[i - 5];
-                    return card
-                }
-                if (movieCard.length >= 5) {
-                    movieBar.lastElementChild.remove();
-                    movieBar.prepend(newFrontCard().firstElementChild);
+                        }
 
-                }
+                        document.querySelectorAll('card').forEach(function (card) {
+                            console.log(card);
+                            card.addEventListener('click', function () {
+                                console.log(this.id);
+                            })
+                        })
 
-                document.querySelectorAll('card').forEach(function (card) {
-                    console.log(card);
-                    card.addEventListener('click', function () {
-                        console.log(this.id);
                     })
-                })
+                    document.querySelectorAll('card').forEach(function (card) {
+                        card.addEventListener('click', function () {
 
+
+                        })
+                    })
+                    document.querySelector('body').style.visibility = 'visible';
+                    body.classList.remove('loadScreen');
+                }
+                fiveCards(movieCard);
             })
-            document.querySelectorAll('card').forEach(function (card) {
-                console.log(card);
-                card.addEventListener('click', function () {
-
-
-
-                })
-            })
-            document.querySelector('body').style.visibility = 'visible';
-            body.classList.remove('loadScreen');
         }
-        fiveCards(movieCard);
-    })
-}
-renderMovies();
+        renderMovies();
 
-// let str = `http://www.omdbapi.com/?t=&apikey=69918388;`
-//
-// function getMovie(titlename){
-//     fetch(`http://www.omdbapi.com/?t=${titlename}&apikey=69918388`).then(res => res.json().then(finale => console.log(finale)));
-//
-//     }
-// getMovie('all');
+
+    }
+    searchMovies(searchBar)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
