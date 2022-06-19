aca85b1b77eb84f5575134044a01ee2a7ced28b6
orgeron.kevin <orgeron.kevin@gmail.com>
2022-06-18 23:08:04 -0500

    added some of the search / add functionality

--- js/my_script.js
+++ js/my_script.js
@@ -112,3 +112,133 @@ const loadMainPageMovieData = (data) => {
     })
 }
 loadMainPageMovieData(mainData);
+
+
+
+// ----------------------------------------------------------------- //
+
+//TODO: THIS IS FOR THE SEARCH MOVIE PORTION
+const onlineMovies = () => {
+    let search = document.querySelector('#search-bar');
+
+    //TODO: This adds click event to search for online movies to add to current page
+    document.querySelector('#search-button').addEventListener('click', () => {
+        console.log(search.value);
+
+        //TODO: Establishing the online search
+        fetch(`http://www.omdbapi.com/?s=${search.value}&page=1&type=movie&apikey=69918388`).then(res => res.json()).then(data => {
+            let movies = data.Search;
+            let moviebar = document.querySelector('movieBar');
+            moviebar.innerHTML = '';
+            console.log(movies);
+
+            //TODO: This adds movies to the main page
+            function addMovie(movie){
+                console.log(movie);
+                fetch("https://truthful-field-mice.glitch.me/movies", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        Title: movie[0].Title,
+                        Poster: movie[0].Poster
+                    }),
+                    headers:{
+                        'Content-Type': 'application/json'
+                    }
+                }).then((res)=> res.json()).then(data => console.log(data))
+            }
+            addMovie(movies);
+
+            //TODO: Adding image to the left side of the page
+            let leftSide = document.querySelector('rightside'); //do not change selector
+            leftSide.innerHTML = `<img style="height: 280px; width: 100%" src="${movies[0].Poster}" alt="failed to load" /> <button id="addMovie" style="justify-self: end; text-align: center" type="button"> a<br>d <br>d</button>`
+
+            //TODO: This is the data to change in order to change the poster data for the cards on button clicks
+            let start = 0;
+            let end = 5;
+            let change = 0;
+
+            //TODO: 5 cards for online search
+            movies.forEach((movie, i) => {
+                if (((i) + start) < end) {
+                    let r = `<card><img style="background-size: contain; height: 100%; width: 100%" src="${movies[(i) + change].Poster}"></card>`
+                    moviebar.innerHTML += r;
+                    //TODO:This loads in page for main page  (corresponds to the above load page functionality)
+                    document.querySelector('body').style.visibility = 'visible';
+                    body.classList.remove('loadScreen');
+                }
+            });
+
+            let btnR = document.querySelector('buttonRight');
+            let btnL = document.querySelector('buttonLeft');
+
+            //TODO: This is needed to hide left button on initial start
+            if (start <= 0) {
+                btnL.style.display = 'none';
+            }
+
+            //TODO: Right button click event
+            btnR.addEventListener('click', () => {
+                moviebar.innerHTML = '';
+                console.log(++start);
+                ++end;
+                ++change;
+
+                if (end >= movies.length) {
+                    btnR.style.display = 'none';
+                } else {
+                    btnR.style.display = 'flex';
+                }
+                if (start <= 0) {
+                    btnL.style.display = 'none';
+                } else {
+                    btnL.style.display = 'flex';
+                }
+
+                movies.forEach((movie, i) => {
+                    if (((i) + start) < end) {
+                        let r = `<card><img style="background-size: contain; height: 100%; width: 100%" src="${movies[(i) + change].Poster}"></card>`
+
+                        moviebar.innerHTML += r;
+                        //TODO:This loads in page for main page  (corresponds to the above load page functionality)
+                        document.querySelector('body').style.visibility = 'visible';
+                        body.classList.remove('loadScreen');
+                    }
+                })
+
+            })
+
+            //TODO: This adds the functionality to the left button
+            btnL.addEventListener('click', () => {
+                moviebar.innerHTML = '';
+                --start;
+                --end;
+                --change;
+
+                if (end >= movies.length) {
+                    btnR.style.display = 'none';
+                } else {
+                    btnR.style.display = 'flex';
+                }
+                if (start <= 0) {
+                    btnL.style.display = 'none';
+                } else {
+                    btnL.style.display = 'flex';
+                }
+
+                movies.forEach((movie, i) => {
+                    if (((i) + start) < end) {
+                        let r = `<card><img style="background-size: contain; height: 100%; width: 100%" src="${movies[(i) + change].Poster}"></card>`
+
+                        moviebar.innerHTML += r;
+
+                        //TODO:This loads in page for main page  (corresponds to the above load page functionality)
+                        document.querySelector('body').style.visibility = 'visible';
+                        body.classList.remove('loadScreen');
+                    }
+                })
+            })
+        });
+    })
+
+}
+onlineMovies();
